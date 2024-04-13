d7ac299abb77b554f3bb58c189bbf97443ede1bd
akshitgaur2005 <akshitgaur@proton.me>
2024-04-13 09:25:53 +0000

    Preprocessing done!

---
+++ processor.py
@@ -0,0 +1,12 @@
+from sklearn import preprocessing
+
+def get_processed(data):
+    X = data.drop(columns = ["SalePrice"])
+    y = data["SalePrice"]
+
+    scaler = preprocessing.StandardScaler()
+
+    X_scaled = scaler.fit_transform(X)
+    y_numpy = y.to_numpy().reshape(1, -1)
+
+    return X_scaled, y_numpy
